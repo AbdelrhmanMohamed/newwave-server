fea203a2321f3c4c6545b60f4b610c96affae17d
AbdelrhmanMohamed <boodyhope@gmail.com>
2025-05-20 11:19:00 +0300

    init

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,27 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
+    },
+  },
+
+  email: {
+    config: {
+      provider: "strapi-provider-email-smtp",
+      providerOptions: {
+        host: "smtp.gmail.com", //SMTP Host
+        port: 465, //SMTP Port
+        secure: true,
+        username: "a.m.abdelzher@gmail.com",
+        password: "sxaj gsyo dxkr wuow",
+        rejectUnauthorized: true,
+        requireTLS: true,
+        connectionTimeout: 1000,
+      },
+    },
+    settings: {
+      defaultFrom: "a.m.abdelzher@gmail.com",
+      defaultReplyTo: "boodyhope@gmail.com",
+    },
+  },
+});
